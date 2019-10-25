d9221d6cde648227e910a10913a612cd48d31490
kendrang <kendrang@users.noreply.github.com>
2019-10-25 15:54:18 -0400

    fixed my dice haha

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,12 +1,31 @@
 public class Dice {
+
+    //    public Integer tossAndSum (Integer numOfDice){
+//        Integer diceResult = 0 ;
+//        for (int i = 1; i <= numOfDice; i++) {
+//             Integer singleDie = (int) (Math.random() * 6 + 1);
+//             diceResult += singleDie;
+//            System.out.println(singleDie);
+//        }return diceResult;
     int numOfDice = 0;
 
-    public Integer rollDice (Integer numOfDice){
-        Integer diceResult = 0 ;
-        for (int i = 1 ; i <=numOfDice; i++) {
-             diceResult= (int) (Math.random() * 6 + 1);
+    public Dice (int numOfDice){
+        this.numOfDice = numOfDice;
+    }
 
-        }return diceResult;
+
+    public int tossAndSum() {
+        int diceResult = 0;
+        for (int i = 1; i <= numOfDice; i++) {
+            diceResult += (int) (Math.random() * 6 + 1);
+        }
+        System.out.println(diceResult);
+
+        return diceResult;
+
+
     }
 
 }
+
+
--- src/main/java/testDice.java
+++ src/main/java/testDice.java
@@ -4,24 +4,9 @@ import org.junit.Test;
 public class testDice {
 
     @Test
-    public void diceTest(){
-        //given
-        boolean actual = false;
-        Dice newdice = new Dice ();
-        int rollingDice = 1;
-        int diceResult = newdice.rollDice(rollingDice);
-        boolean expected = true;
-
-        if (newdice.rollDice(rollingDice) >=1 && newdice.rollDice(rollingDice)<=6){
-            actual= true;
-        }
-        Assert.assertEquals(expected,actual);
-        System.out.println(diceResult);
-
+    public void diceTest() {
+        int sum = Dice.tossAndSum();
 
     }
-
-
-
 }
 
